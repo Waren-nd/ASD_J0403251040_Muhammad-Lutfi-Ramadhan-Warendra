77c05d4031f5aede934b7091ecce3f51b71f652e
Waren-nd <lutfiramadhan0210@gmail.com>
2026-02-11 19:09:45 +0700

    Add files via upload

---
+++ Pertemuan 3/Tugas Praktikum.py
@@ -0,0 +1,174 @@
+#==============================================
+#Pertemuan 3 
+#==============================================
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class CircularSingleLinkedList: 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            self.tail.next = self.head
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.tail.next = self.head
+
+    def delete_node(self, key):
+        if not self.head:
+            print("List is empty")
+            return
+        
+        curr = self.head
+        prev = self.tail
+        
+        # Jika head yang akan dihapus
+        if curr.data == key:
+            # Jika hanya ada satu node
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
+                self.tail.next = self.head
+            return
+        
+        # Mencari node yang akan dihapus
+        curr = curr.next
+        prev = self.head
+        
+        while curr != self.head:
+            if curr.data == key:
+                prev.next = curr.next
+                
+                # Jika yang dihapus adalah tail
+                if curr == self.tail:
+                    self.tail = prev
+                
+                return
+            prev = curr
+            curr = curr.next
+        
+        print("Data tidak ditemukan")
+    def search(self, key):
+        if not self.head:
+            return False
+        
+        curr = self.head
+        
+        while True:
+            if curr.data == key:
+                return True
+            curr = curr.next
+            if curr == self.head:
+                break
+        
+        return False
+    def display(self):
+        if not self.head:
+            print("List is empty")
+            return
+        
+        print("Circular Linked List Traversal:")
+        temp = self.head
+        print(temp.data, end=" -> ")
+        temp = temp.next
+
+        while temp != self.head:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("(null)")
+    
+    def merge(self, list2):
+        # Jika list1 kosong
+        if not self.head:
+            self.head = list2.head
+            self.tail = list2.tail
+            return
+        
+        # Jika list2 kosong
+        if not list2.head:
+            return
+        
+        # Merge list2 ke list1
+        self.tail.next = list2.head
+        self.tail = list2.tail
+        self.tail.next = self.head
+
+
+    
+L = CircularSingleLinkedList()
+L.insert_at_end(3)
+L.insert_at_end(5)
+L.insert_at_end(13)
+L.insert_at_end(2)
+L.display()
+
+# LATIHAN 1
+L.delete_node(5)
+L.display()
+
+#LATIHAN 2
+CSL1 = CircularSingleLinkedList()
+CSL1.insert_at_end(3)
+CSL1.insert_at_end(7)
+CSL1.insert_at_end(12)
+CSL1.insert_at_end(19)
+CSL1.insert_at_end(25)
+CSL1.display()
+
+#x = int(input("Masukkan elemen yang ingin dicari: "))
+#match CSL1.search(x):
+#    case True:
+#        print("Elemen ditemukan.")
+#   case False:
+#        print("Elemen tidak ditemukan.")
+
+#LATIHAN 4
+LL1 = CircularSingleLinkedList()
+x = [1, 3, 5, 7]
+for data in x:
+    LL1.insert_at_end(data)
+
+LL2 = CircularSingleLinkedList()
+y = [2, 4, 6, 8]
+for data in y:
+    LL2.insert_at_end(data)
+
+input1 = input("Masukkan elemen untuk Linked List 1: ")
+
+if input1.strip() != "":
+    data1 = input1.split(",")
+    for data in data1:
+        LL1.insert_at_end(int(data.strip()))
+
+# Input Linked List 2
+input2 = input("Masukkan elemen untuk Linked List 2: ")
+
+if input2.strip() == "":
+    print("Masukkan elemen untuk Linked List 2: (Tidak ada elemen)")
+else:
+    data2 = input2.split(",")
+    for data in data2:
+        LL2.insert_at_end(int(data.strip()))
+
+print("Linked List 1:")
+LL1.display()
+
+print("Linked List 2:")
+LL2.display()
+
+LL1.merge(LL2)
+
+print("Linked List setelah digabungkan:")
+LL1.display()
+
